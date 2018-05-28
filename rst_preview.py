a3e5e38d9a84bef591f94046694ceb97e3bc9249
tonyriverms <tzchen86@gmail.com>
2018-05-27 20:53:33 -0400

    The simple python script for sphinx live preview.
    more comments

--- rst_preview.py
+++ rst_preview.py
@@ -7,18 +7,20 @@ from selenium import webdriver
 
 # region options for the live preview
 
-path_to_webdriver = "D:/Program Files/webdrivers/chromedriver.exe"
-refresh_delay = 0.2
-render_math = True
+path_to_webdriver = "D:/Program Files/webdrivers/chromedriver.exe"  # This is the path to the webdriver. Change this to where you put the driver.
+update_delay = 0.2  # The update delay when some file change is detected.
+render_math = True  # True if you want to re-render math every time an update is made; otherwise False
+source_folder_name = "source"
+build_folder_name = "build"
 
-driver = webdriver.Chrome(path_to_webdriver)
+driver = webdriver.Chrome(path_to_webdriver)  # Change "Chrome" to the right type of browser driver you are using
 
 # endregion
 
 script_path = dirname(realpath(__file__))
-source_path = join(script_path, "source")
+source_path = join(script_path, source_folder_name)
 source_path_len = len(source_path) + len(pathsep)
-build_path = join(script_path, "build", "html")
+build_path = join(script_path, build_folder_name, "html")
 rst_files = glob(join(script_path, '**/*.rst'), recursive=True)
 size_dict = {}
 for rst_file in rst_files:
@@ -30,10 +32,10 @@ while True:
             record = size_dict[rst_file]
             new_size = getsize(rst_file)
             new_mtime = getmtime(rst_file)
-            if record[0] != new_size or record[1] < new_mtime:
+            if record[0] != new_size or record[1] < new_mtime:  # Checks if file size or latest modify time change
                 html_file = join(build_path, rst_file[source_path_len: -4] + ".html")
-                call("sphinx-build -M html source build")
-                if record[2] == 0:
+                call("sphinx-build -M html %s %s" % (source_folder_name, build_folder_name))
+                if record[2] == 0:  # open the corresponding html for the rst file under editing
                     driver.get(html_file)
                 else:
                     with open(html_file, 'r', encoding="utf-8") as content_file:
@@ -41,7 +43,7 @@ while True:
                     new_body_start_idx = content.index("<body")
                     new_body_start_idx = content.index(">", new_body_start_idx) + 1
                     content = content[new_body_start_idx:content.rindex("</body>")].strip()
-                    driver.execute_script('document.getElementsByTagName("body")[0].innerHTML = arguments[0]', content)
+                    driver.execute_script('document.getElementsByTagName("body")[0].innerHTML = arguments[0]', content)  # make the update
                     if render_math:
                         driver.execute_script('MathJax.Hub.Queue(["Typeset",MathJax.Hub])')
 
@@ -49,4 +51,4 @@ while True:
                 record[1] = new_mtime
                 record[2] += 1
 
-    sleep(refresh_delay)
+    sleep(update_delay)
